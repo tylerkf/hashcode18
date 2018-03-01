c6abb5dde6aac82bb232a7ee9d21e93598c8f6c5
Harvey <harveywilliams@live.com>
2018-03-01 20:17:30 +0000

    Nearly there

--- simulation.py
+++ simulation.py
@@ -1,3 +1,4 @@
+
 f = open("a_example.in", "r", encoding="utf-8")
 first = True
 paramsData = list(map(int,f.readline().split(' '))) # R,C,F,N,B,T
@@ -26,17 +27,23 @@ class Ride:
         end_pos   = [vals[2], vals[3]]
         start_time= vals[4]
         end_time  = vals[5]
+    def distance(self):
+        return abs(start_pos[0]-end_pos[0])+abs(start_pos[1]-end_pos[1])
 
 class Vehicle:
     def __init__(self,id):
         self.id = id
-        self.assignedRide = 0
+        self.assignedRides = []
+        self.currentRideId = 0
         self.pos = [0,0]
 
-        self.intentState = 2 # 0 = Going to start, 1 = Going to end, 2 = idle
+        self.intentState = 2 # 0 = Going to start, 1 = Going to end, 2 = Waiting, 3 = Done
         self.movementState = 0 # 0 = Going across, 1 = Going vertical
 
     def assign(self,ride):
+        self.assignedRides.append(ride)
+
+    def next(self,ride):
         self.assignedRide = ride
         if self.pos == ride.start_pos:
             self.intentState = 1
@@ -47,12 +54,14 @@ class Vehicle:
             else:
                 self.movementState = 1
 
-    def step(self):
+    def step(self,currentTime):
         target = []
         if self.intentState == 0:
-            target = self.assignedRide.start_pos
+            target = self.assignedRides[currentRideId].start_pos
         elif self.intentState == 1:
-            target = self.assignedRide.end_pos
+            target = self.assignedRide[currentRideId].end_pos
+        elif self.intentState = 2:
+            # Check if start next ride
         else:
             return
 
@@ -85,15 +94,23 @@ rides = [0] * paramsData[3]
 grid = Grid(paramsData,ridesData)
 vehicles = [Vehicle(i) for i in range(paramsData[2])]
 
-def simulate(rides,grid,schedule):
+def simulate(rides,grid,vehicles,schedule):
     score = 0
+
+    # Setting up each vehicle
+    for vehicleId in range(0,len(vehicles)):
+        vehicleSchedule = schedule[vehicleId]
+        M = vehicleSchedule[0]
+        for i in range(1,M+1):
+            vehicles[vehicleId].assign(rides[vehicleSchedule[i]])
+
     
     for t in range(0,grid.max_time): # Time step
-        for vehicleId in range(0,len(vehicles)): # Loop through each vehicle
-            print(vehicles[vehicleId])
-
-
-simulate(rides,grid,0)
+        for vehicleId in range(0,len(vehicles)):
+            vehicles[vehicleId].step(t)
+            
+               
+simulate(rides,grid,vehicles,0)
             
             
 
